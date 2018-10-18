c5ec6c2354d40745bc9f6a61191cda95d14eb7f2
Marcial Rosales <mrosales@pivotal.io>
2018-10-18 12:21:44 +0200

    Diagnostics tests that validate plugins config
    [#160928591]

--- src/diagnostics/configuration_test.bash
+++ src/diagnostics/configuration_test.bash
@@ -7,8 +7,15 @@ TEST="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # basht macro, shellcheck fix
 export T_fail
 
+# IMPORTs section
 # shellcheck disable=SC1090
 source $TEST/test_helpers
+
+# shellcheck disable=SC1090
+source $TEST/rabbitmq-plugins_helpers
+
+# shellcheck disable=SC1090
+source $TEST/rabbitmq-job_helpers
 
 
 T_ConfiguredErlangVersionIsRunning() {
@@ -27,4 +34,24 @@ T_ConfiguredRabbitMQVersionIsRunning() {
   expected="${RABBITMQ_SERVER_PACKAGE_VERSION:?must be set}"
 
   expect_to_equal "${actual}" "\"${expected}\"" || $T_fail "RabbitMQ version '${actual}' does not match expected version '${expected}'"
+}
+
+allPluginsShouldBeEnabled() {
+  expect_to_contain "$(rabbitmq-plugins-installed-list | tr '\n' ',')" "$(rabbitmq-plugins-all-enabled-list | tr '\n' ',')"
+}
+
+allConfiguredPluginsShouldBeEnabled() {
+  for plugin in $(rabbitmq-job-OnlyEnableThesePlugins); do expect_to_contain "$(rabbitmq-plugins-all-enabled-list)" "$plugin" ; done
+}
+allExplicitlyEnabledShouldBeConfigured() {
+  for plugin in $(rabbitmq-plugins-explicitly-enabled-list); do expect_to_contain "$(rabbitmq-job-OnlyEnableThesePlugins)" "$plugin" ; done
+}
+
+T_ConfiguredPluginsAreEnabled() {
+  if [ -z "$(rabbitmq-job-OnlyEnableThesePlugins)" ]; then
+    allPluginsShouldBeEnabled
+  else
+    allConfiguredPluginsShouldBeEnabled
+    allExplicitlyEnabledShouldBeConfigured
+  fi
 }
