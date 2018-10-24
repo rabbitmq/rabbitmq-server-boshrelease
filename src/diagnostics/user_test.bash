19cb1ddfc04aa52b75f258493f04409b204c6a0c
Marcial Rosales <mrosales@pivotal.io>
2018-10-24 17:28:04 +0200

    Check that user topic permissions are preserved
    This is part of the diagnostic that checked that all users and their
    permissions are preserved

--- src/diagnostics/user_test.bash
+++ src/diagnostics/user_test.bash
@@ -27,12 +27,23 @@ AllSnapshotUserPermissionsStillExist() {
     vhost_missing_permissions="$(comm -23 <(store_permissions $vhost) <(rabbitmq_permissions $vhost) | awk '{ print $1 }' | tr '\n' ' ')"
     [[ -z $vhost_missing_permissions ]] || missing_permissions+="{ $vhost :: $vhost_missing_permissions } , "
   done
-  [[ -z $missing_permissions ]] || $T_fail "There are missing user permissions ( {vhost, users} ): $missing_permissions"
+  [[ -z $missing_permissions ]] || $T_fail "There are missing user permissions { vhost :: user } : $missing_permissions"
+}
+AllSnapshotUserTopicPermissionsStillExist() {
+  local missing_topic_permissions
+  for vhost in $(store_vhosts)
+  do
+    local vhost_missing_topic_permissions
+    vhost_missing_topic_permissions="$(comm -23 <(store_topic_permissions $vhost) <(rabbitmq_topic_permissions $vhost) | awk '{ print $1"-"$2 }' | tr '\n' ' ')"
+    [[ -z $vhost_missing_topic_permissions ]] || missing_topic_permissions+="{ $vhost :: $vhost_missing_topic_permissions } , "
+  done
+  [[ -z $missing_topic_permissions ]] || $T_fail "There are missing user topic permissions { vhost :: user-topic }  : $missing_topic_permissions"
 }
 T_AllSnapshotUsersStillExist() {
   if store_users_exists; then
     AllSnapshotUserTagsStillExist
     AllSnapshotUserPermissionsStillExist
+    AllSnapshotUserTopicPermissionsStillExist
   else
     echo "There is no snapshot users in store"
   fi
