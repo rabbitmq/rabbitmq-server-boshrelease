ef099f550af4c3ee02d08737c1554743dffdc2d9
Marcial Rosales <mrosales@pivotal.io>
2018-10-31 10:30:44 +0100

    Check AMQP port availability rather that using python to connect
    We used to launch a python app that connected to RabbitMQ AMQP port.
    This app required python3  pip and pika library installed.
    With this commit we are replacing the python client with a simple port
    check using nc utility which is already available in the release.
    
    [#160928638]

---
+++ src/diagnostics/amqp/amqp_test.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+[[ $DEBUG = true ]] && set -x
+
+TEST="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# basht macro, shellcheck fix
+export T_fail
+
+# IMPORTs section
+# shellcheck disable=SC1090
+source $TEST/test_helpers
+
+# shellcheck disable=SC1090
+source $TEST/rabbitmq_helpers
+
+T_RabbitMQIsReadyToServiceAMQP() {
+  nc -vz "$(hostname -I)" 5672
+}
